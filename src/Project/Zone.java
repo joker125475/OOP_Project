f2062c48737c118fa0ad697949d4a9c833f6bad7
2023170545 <163653726+2023170545@users.noreply.github.com>
2024-12-05 18:55:44 +0200

    Update Zone.java

--- src/Project/Zone.java
+++ src/Project/Zone.java
@@ -1,4 +1,60 @@
 package Project;
-
+import java.util.Arrays;
 public class Zone {
+  //there is problem because trafficofficer class inherits from zone
+
+    private int id;
+    private String name;
+    private String location;
+    private Traffic_Light[]t;
+
+
+    public Zone(int id, String name, String location, Traffic_Light[] t) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.t = t;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Traffic_Light[] getT() {
+        return t;
+    }
+
+    public void setT(Traffic_Light[] t) {
+        this.t = t;
+    }
+
+    @Override
+    public String toString() {
+        return "Zone{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", trafficLights=" + Arrays.toString(t) +
+                '}';
+    }
 }
