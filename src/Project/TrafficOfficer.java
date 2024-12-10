52e99530bd83402f5ea9ea6a8a4159c094310536
2023170545 <163653726+2023170545@users.noreply.github.com>
2024-12-10 09:28:45 +0200

    Update TrafficOfficer.java

--- src/Project/TrafficOfficer.java
+++ src/Project/TrafficOfficer.java
@@ -3,23 +3,42 @@ import java.util.List;
 import java.util.ArrayList;
 public class TrafficOfficer {
 
+    private String ID;
+    private String name;
+    private String contact_info;
+    private String assigned_zone;
 
+    public String getID() {
+        return ID;
+    }
 
+    public void setID(String ID) {
+        this.ID = ID;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getContact_info() {
+        return contact_info;
+    }
 
+    public void setContact_info(String contact_info) {
+        this.contact_info = contact_info;
+    }
 
+    public String getAssigned_zone() {
+        return assigned_zone;
+    }
 
-
-
-
-
-
-
-
-
-
+    public void setAssigned_zone(String assigned_zone) {
+        this.assigned_zone = assigned_zone;
+    }
   
   //traffic officer functions
     public void record_violation(Vehicle V,TrafficViolation violation, String date){
