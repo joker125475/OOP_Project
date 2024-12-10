7bf4f5d93e274329a7e911ee0f8c34658ced084b
TonyFarag90 <163879605+TonyFarag90@users.noreply.github.com>
2024-12-10 07:16:19 +0200

    .

--- src/Project/Traffic_Light.java
+++ src/Project/Traffic_Light.java
@@ -1,16 +1,20 @@
 package Project;
 
 public class Traffic_Light {
-    private int id;
+    private int id=0;
     private String location;
-    private String status;
-    private int duration;
+    private int greenDuration;
+    private int yellowDuration;
+    private int redDuration;
+    private String currentState;
 
-    public Traffic_Light(int id, String location, String status, int duration) {
+    public Traffic_Light(int id, String location, int greenDuration, int yellowDuration, int redDuration, String currentState) {
         this.id = id;
         this.location = location;
-        this.status = status;
-        this.duration = duration;
+        this.greenDuration = greenDuration;
+        this.yellowDuration = yellowDuration;
+        this.redDuration = redDuration;
+        this.currentState = currentState;
     }
 
     public int getId() {
@@ -29,50 +33,47 @@ public class Traffic_Light {
         this.location = location;
     }
 
-    public String getStatus() {
-        return status;
+    public int getGreenDuration() {
+        return greenDuration;
     }
 
-    public void setStatus(String status) {
-        if(status.equalsIgnoreCase("Red")||
-                status.equalsIgnoreCase("Green")
-        ||status.equalsIgnoreCase("Yellow")){
-            this.status = status;
-
-        }
-        else
-            throw new IllegalArgumentException("Invalid Status.It should be green or yellow or red");
+    public void setGreenDuration(int greenDuration) {
+        this.greenDuration = greenDuration;
     }
 
-    public int getDuration() {
-        return duration;
+    public int getYellowDuration() {
+        return yellowDuration;
     }
 
-    public void setDuration(int duration) {
-        if(duration>0)
-            this.duration = duration;
-        else
-            throw new IllegalArgumentException("Duration must be greater than 0");
+    public void setYellowDuration(int yellowDuration) {
+        this.yellowDuration = yellowDuration;
+    }
 
+    public int getRedDuration() {
+        return redDuration;
     }
-    public void nextcycle(){
-        switch (status.toLowerCase()){
-            case "red":
-                status="green";
-                break;
-            case "green":
-                status="yellow";
-                break;
-            case "yellow":
-                status="red";
-                break;
-        }
+
+    public void setRedDuration(int redDuration) {
+        this.redDuration = redDuration;
     }
+
+    public String getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(String currentState) {
+        this.currentState = currentState;
+    }
+
     @Override
-    public String toString(){
-        return "Traffic Light[id="+id+
-                ",Location is"+location+
-                ",status is"+status+
-                ",Duration="+duration+"seconds";
+    public String toString() {
+        return "Traffic_Light{" +
+                "id=" + id +
+                ", location='" + location + '\'' +
+                ", greenDuration=" + greenDuration +
+                ", yellowDuration=" + yellowDuration +
+                ", redDuration=" + redDuration +
+                ", currentState='" + currentState + '\'' +
+                '}';
     }
 }
